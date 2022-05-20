0e632f975072854f4fa692be6ca02aaf5b6e1435
agungperdana <agung.dodi.perdana@gmail.com>
2022-05-20 17:38:29 +0700

    add gateway-service

---
+++ gateway-service/src/main/java/com/kratonsolution/component/gateway/service/GatewayServiceEntryPoint.java
@@ -0,0 +1,12 @@
+package com.kratonsolution.component.gateway.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayServiceEntryPoint {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServiceEntryPoint.class, args);
+    }
+}
